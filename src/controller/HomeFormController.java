3fa32145091fdd0dacc58d44e193f4c99e1ad827
manojiS02 <salakshanamanoji@gmail.com>
2023-04-13 01:18:42 +0530

    Added initial Project

---
+++ src/controller/HomeFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class HomeFormController {
+    public void btnRemainKeyMoneyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReservationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void chbxShowOnAction(ActionEvent actionEvent) {
+    }
+}
