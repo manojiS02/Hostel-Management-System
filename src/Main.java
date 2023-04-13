381f7c506e709f93f9237bc28f16e919e7b83d37
manojiS02 <salakshanamanoji@gmail.com>
2023-04-13 16:19:48 +0530

    Added initial Project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("view/LoginForm.fxml"))));
+        primaryStage.setTitle("Hostel Management System");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
--- src/controller/HomeFormController.java
+++ src/controller/HomeFormController.java
@@ -1,20 +1,47 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeFormController {
+    public AnchorPane background;
+    public JFXButton btnStudent;
+    public JFXButton btnRoom;
+    public JFXButton btnRemainKeyMoney;
+    public JFXButton BtnReservation;
+    public JFXButton btnBack;
+    public AnchorPane context;
+
     public void btnRemainKeyMoneyOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnReservationOnAction(ActionEvent actionEvent) {
+    public void btnReservationOnAction(ActionEvent actionEvent) throws IOException {
+        context.getChildren().clear();
+        Parent parent=FXMLLoader.load(getClass().getResource("../view/ReservationForm.fxml"));
+        context.getChildren().add(parent);
     }
 
-    public void btnBackOnAction(ActionEvent actionEvent) {
+    public void btnBackOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) background.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
     }
 
-    public void btnRoomOnAction(ActionEvent actionEvent) {
+    public void btnRoomOnAction(ActionEvent actionEvent) throws IOException {
+        context.getChildren().clear();
+        Parent parent=FXMLLoader.load(getClass().getResource("../view/RoomForm.fxml"));
+        context.getChildren().add(parent);
     }
 
-    public void btnStudentOnAction(ActionEvent actionEvent) {
+    public void btnStudentOnAction(ActionEvent actionEvent) throws IOException {
+        context.getChildren().clear();
+        Parent parent= FXMLLoader.load(getClass().getResource("../view/StudentForm.fxml"));
+        context.getChildren().add(parent);
     }
 }
